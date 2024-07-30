c7117578ef256a29155a2e217455c1a2e75b0b9e
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-07-30 12:57:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.classList.contains('show')) {
+        navLinks.classList.remove('show');
+    } else {
+        navLinks.classList.add('show');
+    }
+});
